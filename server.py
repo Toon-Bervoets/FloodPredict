8c092e4db6e5612e2b0de075f1c2aa6ae742c8b7
Toon Bervoets <toon.bervoets1@student.kuleuven.be>
2024-10-15 20:51:54 +0200

    added layout 1.0

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from weather import get_current_weather
 from waitress import serve
+from datetime import datetime, timezone, timedelta
 
 app = Flask(__name__)
 
@@ -26,12 +27,19 @@ def get_weather():
     if not weather_data['cod'] == 200:
         return render_template('city-not-found.html')
 
+    # Calculate local time based on timezone and dt (time of data calculation)
+    timezone_offset = timedelta(seconds=weather_data['timezone'])  # Shift in seconds from UTC
+    utc_time = datetime.fromtimestamp(weather_data['dt'], tz=timezone.utc)
+    local_time = utc_time + timezone_offset
+
     return render_template(
         "weather.html",
         title=weather_data["name"],
         status=weather_data["weather"][0]["description"].capitalize(),
         temp=f"{weather_data['main']['temp']:.1f}",
-        feels_like=f"{weather_data['main']['feels_like']:.1f}"
+        feels_like=f"{weather_data['main']['feels_like']:.1f}",
+        local_time=local_time.strftime('%d-%m-%Y %H:%M'),
+        weather_icon_url = f"http://openweathermap.org/img/wn/{weather_data['weather'][0]['icon']}@2x.png"
     )
 
 
